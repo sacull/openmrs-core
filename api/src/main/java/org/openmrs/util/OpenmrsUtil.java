77e1dec493912784ee73ecfbb86c9d89f58f31c3
Łukasz Dębicki <sacull@gmail.com>
2020-08-12 17:13:33 +0200

    TRUNK-5849: Correct comment

--- api/src/main/java/org/openmrs/util/OpenmrsUtil.java
+++ api/src/main/java/org/openmrs/util/OpenmrsUtil.java
@@ -1165,7 +1165,7 @@ public class OpenmrsUtil {
 	 * @throws ApplicationContextException if the current JVM version is earlier than Java 8
 	 */
 	public static void validateJavaVersion() {
-		// check whether the current JVM version is at least Java 8
+		// checks whether the current JVM version is at least Java 8
 		if (JdkVersion.getJavaVersion().matches("1.(0|1|2|3|4|5|6|7).(.*)")) {
 			throw new APIException("OpenMRS requires Java 8 and above, but is running under " + JdkVersion.getJavaVersion());
 		}
